32d6e1f504577823b890bbf446ce2531f8da3624
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2021-05-24 19:35:38 +0100

    Vue CLI install

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+
+    <div class="row">
+      <div class="col-lg-4">
+        1
+      </div>
+
+      <div class="col-lg-4">
+        2
+      </div>
+      <div class="col-lg-4">
+        3
+      </div>
+    </div>
+
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
